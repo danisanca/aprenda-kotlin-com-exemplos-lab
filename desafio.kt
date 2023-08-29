31edb13880573d2cd3ab0c114f2d2fc063578650
Patrick Soares de Souza Silva <dani_sanca@hotmail.com>
2023-08-29 18:49:42 -0300

    Finalização do desafio

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,29 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+data class Usuario(val nome: String, val email:String)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class NivelFormacao { BASICO, INTERMEDIARIO, AVANÇADO, EXPERT }
+data class Aulas(val nome: String, val urlVideo:String?, val urlConteudoLeitura:String?)
+data class ConteudoEducacional(val nome: String, val duracaoMinutos: Int?, val aulas: List<Aulas>)
 
-class Usuario
+data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>,val nivel:NivelFormacao) {
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
-
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
-    val inscritos = mutableListOf<Usuario>()
+    val inscritos: MutableList<Usuario> = mutableListOf()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+
+    val daniel:Usuario = Usuario("daniel","dani@hotmail.com")
+    val aula1:Aulas = Aulas("Declarando Variaveis No Kotlin","http://videoaula1.mp4","http://github.com/doc1.md")
+    val aula2:Aulas = Aulas("Declarando Funções No Kotlin","http://videoaula2.mp4","http://github.com/doc2.md")
+    val kotlinModulo1:ConteudoEducacional = ConteudoEducacional("Modulo 1",1200,aulas= listOf(aula1))
+    val kotlinModulo2:ConteudoEducacional = ConteudoEducacional("Modulo 2",2400,aulas= listOf(aula2))
+    val formacaoKotlin:Formacao = Formacao("Aprendendo Kotlin Na Pratica", listOf(kotlinModulo1,kotlinModulo2),NivelFormacao.BASICO)
+
+    formacaoKotlin.matricular(daniel)
+
+    println(formacaoKotlin)
+
 }
